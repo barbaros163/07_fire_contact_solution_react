ec5798dc28bde6359d2f57114393ec2b5f45b00a
Yasin Yavuz <barbaros1675@gmail.com>
2022-11-20 11:26:26 +0100

    database solution check it again

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,13 +1,17 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyApJXH5o1PXazmK6AU_qFsAo3gmdRdzZCI",
-  authDomain: "fire-contact-project-12.firebaseapp.com",
-  databaseURL: "https://fire-contact-project-12-default-rtdb.firebaseio.com",
-  projectId: "fire-contact-project-12",
-  storageBucket: "fire-contact-project-12.appspot.com",
-  messagingSenderId: "454240655526",
-  appId: "1:454240655526:web:32b3ad91caf88c5fa6fc67"
+  apiKey: "AIzaSyCj5MVJGZh5xmDyJj7mNS62EE-1Ueg_Yy0",
+  authDomain: "fire-contact-projectt.firebaseapp.com",
+  databaseURL: "https://fire-contact-projectt-default-rtdb.firebaseio.com",
+  projectId: "fire-contact-projectt",
+  storageBucket: "fire-contact-projectt.appspot.com",
+  messagingSenderId: "263207275543",
+  appId: "1:263207275543:web:6e3bb6add1e67533b26052",
 };
 
 // Initialize Firebase
--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,64 +1,66 @@
 // ADD USER
 
-import { getDatabase, ref, set,push, onValue, remove,update } from "firebase/database";
-import firebase from "./firebase"
-import {useState,useEffect} from "react"
+import {
+  getDatabase,
+  ref,
+  set,
+  push,
+  onValue,
+  remove,
+  update,
+} from "firebase/database";
+import firebase from "./firebase";
+import { useState, useEffect } from "react";
 import Toastify from "./toastify";
 
+export const AddUser = (info) => {
+  const db = getDatabase(firebase);
+  const userRef = ref(db, "users/");
+  const newUserRef = push(userRef);
 
-export const AddUser=(info)=>{
-    const db=getDatabase(firebase)
-    const userRef=ref(db,"user/")
-    const newUserRef=push(userRef)
-
-    set(newUserRef,{
-        username:info.username,
-        phoneNumber:info.phoneNumber,
-        gender:info.gender
-    })
-  
-}
+  set(newUserRef, {
+    username: info.username,
+    phoneNumber: info.phoneNumber,
+    gender: info.gender,
+  });
+};
 
 // READ INFO
-export const useFetch=()=>{
-    const [isLoading,setIsLoading]=useState(true)
-    const [contactList,setContactList]= useState()
-    useEffect(() => {
-        const db=getDatabase(firebase)
-        const userRef=ref(db,"user/")
-
-        onValue(userRef,(snapshot)=>{
-            const data = snapshot.val();
-            const userArray=[]
-
-            for(let id in data){
-                userArray.push({id,...data[id]})
-            }
-            setContactList(userArray)
-            setIsLoading(false)
-        })
-       
-    },[])
-    return {isLoading,contactList}
-
-}
-
-export const DeleteUser=(id)=>{
+export const useFetch = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [contactList, setContactList] = useState();
+  useEffect(() => {
     const db = getDatabase(firebase);
-    // const userRef=ref(db,"user/")
-    remove(ref(db,"user/"+id))
-    Toastify("Deleted Successfully")
+    const userRef = ref(db, "users/");
 
-}
+    onValue(userRef, (snapshot) => {
+      const data = snapshot.val();
+      const userArray = [];
 
-export const UpdateUser=(info)=>{
-    const db=getDatabase(firebase)
-    const userRef=ref(db,"user/")
+      for (let id in data) {
+        userArray.push({ id, ...data[id] });
+      }
+      setContactList(userArray);
+      setIsLoading(false);
+    });
+  }, []);
+  return { isLoading, contactList };
+};
 
-    const updates = {};
+export const DeleteUser = (id) => {
+  const db = getDatabase(firebase);
+  // const userRef=ref(db,"user/")
+  remove(ref(db, "users/" + id));
+  Toastify("Deleted Successfully");
+};
 
-    updates["user/"+info.id]=info
+export const UpdateUser = (info) => {
+  const db = getDatabase(firebase);
+  const userRef = ref(db, "users/");
 
-    return update(ref(db), updates);
+  const updates = {};
 
-}
+  updates["user/" + info.id] = info;
+
+  return update(ref(db), updates);
+};
